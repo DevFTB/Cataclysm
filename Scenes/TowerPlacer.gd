ced0f0d9d6cdb6fadd2d069cbeb0bb651c74a4fd
Your Name <indegamedev@gmail.com>
2022-10-01 14:19:05 +1000

    cannot place on top over other towers

--- Scenes/TowerPlacer.gd
+++ Scenes/TowerPlacer.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var tower_parent : NodePath
 
 var can_place : bool = false
+var obstructed : bool  = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,8 +18,7 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("target"):
-			if can_place:
-				print(can_place)
+			if can_place and not obstructed and tower != null:
 				place_tower()
 
 func place_tower():
@@ -26,9 +26,18 @@ func place_tower():
 	get_node(tower_parent).add_child(new_tower)
 
 	new_tower.position = get_viewport().get_mouse_position()
-
+	new_tower.connect("mouse_entered", on_Tower_mouse_entered)
+	new_tower.connect("mouse_exited", on_Tower_mouse_exited)
+	
 	get_node("AudioStreamPlayer").play()
 
 func _on_placeable_area_can_place_changed(can_place):
 	self.can_place = can_place
 	pass # Replace with function body.
+
+func on_Tower_mouse_entered():
+	self.obstructed = true
+	pass
+func on_Tower_mouse_exited():
+	self.obstructed = false
+	pass
